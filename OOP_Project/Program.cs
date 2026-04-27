e39e7f9ef80df8406c2dba27d3045de2447fc756
Basiliksys <belloslav@gmail.com>
2026-04-27 23:14:03 +0300

    Fixed problem with outputing ukrainian symbols.

--- OOP_Project/Program.cs
+++ OOP_Project/Program.cs
@@ -1,7 +1,11 @@
-﻿class Program
+﻿
+class Program
 {
+
     static void Main(string[] args)
     {
+        Console.OutputEncoding = System.Text.Encoding.Unicode; // Unicode is UTF-16LE
+        Console.InputEncoding = System.Text.Encoding.Unicode;
         // Обов'язковий вивід згідно з вимогами Версії 1
         Console.WriteLine("ПІБ студента: Іванов Іван Іванович, курс: 2, група: КН-21");
         Console.WriteLine("Варіант завдання: Kyiv Smart City School");
