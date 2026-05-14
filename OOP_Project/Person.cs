781c3038d3aa75690bd8c2dfd19d909b51e9ebc6
Basiliksys <belloslav@gmail.com>
2026-05-14 11:54:42 +0300

    Created abstract base class 'Person' with abstract method 'DisplayRole()'.  Created interface 'IActivityParticipant' wich will be implemented in Student and Teacher.

---
+++ OOP_Project/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public abstract class Person
+{
+    public string FullName { get; set; }
+
+    // Конструктор базового класу
+    protected Person(string fullName)
+    {
+        FullName = fullName;
+        Console.WriteLine($"[Базовий клас Person] Створено людину: {FullName}");
+    }
+
+    // Абстрактний метод, який обов'язково треба перевизначити у похідних класах
+    public abstract void DisplayRole();
+}
+
+public interface IActivityParticipant
+{
+    void PerformActivity(); // Метод, який мають реалізувати учасники платформи
+}
