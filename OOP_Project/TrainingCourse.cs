edd112755669ae6cadb7f844703ad1c9441ff7ce
Basiliksys <belloslav@gmail.com>
2026-05-14 20:01:11 +0300

    Integrated try-catch-finally block to training course. Added file I/O operations. Demonstrated standard exceptions in training course.

--- OOP_Project/TrainingCourse.cs
+++ OOP_Project/TrainingCourse.cs
@@ -5,19 +5,34 @@ using System.Text;
 public class TrainingCourse
 {
     public string CourseName { get; set; }
+    public int MaxCapacity { get; set; }
     public Teacher CourseTeacher { get; set; }
     public List<Student> EnrolledStudents { get; set; }
 
-    public TrainingCourse(string courseName, Teacher teacher)
+    public TrainingCourse(string courseName, Teacher teacher, int maxCapacity)
     {
         CourseName = courseName;
         CourseTeacher = teacher;
+        MaxCapacity = maxCapacity;
         EnrolledStudents = new List<Student>();
     }
 
     public void AddStudent(Student student)
     {
+        // ГЕНЕРАЦІЯ ВИНЯТКУ: Перевищення ліміту курсу
+        if (EnrolledStudents.Count >= MaxCapacity)
+        {
+            throw new CourseCapacityExceededException($"На курсі '{CourseName}' закінчилися вільні місця (Ліміт: {MaxCapacity}).");
+        }
         EnrolledStudents.Add(student);
-        Console.WriteLine($"[Курс] Учня {student.FullName} успішно додано на курс '{CourseName}'.");
+        Console.WriteLine($"[Курс] {student.FullName} успішно зарахований на '{CourseName}'.");
+    }
+
+    // Демонстрація можливого ділення на нуль
+    public void CalculateAverageAttendance(int totalVisits, int heldLessons)
+    {
+        // Якщо уроків не було (heldLessons = 0), система викине DivideByZeroException
+        int avg = totalVisits / heldLessons;
+        Console.WriteLine($"Середня відвідуваність: {avg} учнів на урок.");
     }
 }
